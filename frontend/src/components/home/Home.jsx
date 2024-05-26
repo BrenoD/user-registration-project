cb6d9401c6066cc7d6af82bc3641d1fb516d88f6
BrenoD <dias2579@gmail.com>
2024-05-26 11:58:11 -0300

    Users registration

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props => 
+    <Main icon="home" title="Inicio"
+        subtitle="Segundo projeto de React">
+            <div className='display-10'>Bem vindo</div>
+            <hr />
+            <p className='mb-0'>Sistema para exemplificar a contrucao
+                de cadastro em react</p>
+        </Main>
